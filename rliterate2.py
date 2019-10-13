de5789d005828277857af0e6f806affdea2c7f8e
Rickard Lindberg <ricli85@gmail.com>
2019-10-13 09:19:17 +0200

    Can lay out widgets once.

--- rliterate2.py
+++ rliterate2.py
@@ -6,29 +6,30 @@ import wx
 
 class RLGuiMixin(object):
 
-    def __init__(self):
-        self.props = {}
-        self.props.update(self._get_defaults())
+    def __init__(self, props):
+        self._props = {}
+        self._update_props(props)
         self._create_gui()
 
-    def _get_defaults(self):
-        return {}
-
     def UpdateProps(self, props):
-        changed = False
-        for key, value in props.items():
-            if self._prop_differs(key, value):
-                self.props[key] = value
-                changed = True
-        if changed:
+        if self._update_props(props):
             self._update_gui()
 
+    def _update_props(self, props):
+        self._changed_props = []
+        for p in [props, self._get_props()]:
+            for key, value in p.items():
+                if self._prop_differs(key, value):
+                    self._props[key] = value
+                    self._changed_props.append(key)
+        return len(self._changed_props) > 0
+
     def _prop_differs(self, key, value):
-        if key not in self.props:
+        if key not in self._props:
             return True
-        if self.props[key] is value:
+        if self._props[key] is value:
             return False
-        if self.props[key] == value:
+        if self._props[key] == value:
             return False
         return True
 
@@ -41,26 +42,84 @@ class RLGuiMixin(object):
 class RLGuiContainerMixin(RLGuiMixin):
 
     def _create_gui(self):
+        self.Sizer = self._create_sizer()
         self._children = []
+        self._create()
 
     def _update_gui(self):
+        self._create()
+        self.Layout()
+        self.Refresh()
+
+    def _update_builtin(self):
+        for name in self._changed_props:
+            if name == "background":
+                self.SetBackgroundColour(self._props["background"])
+
+    def _create(self):
+        self._update_builtin()
         self._sizer_index = 0
         self._child_index = 0
+        self._create_widgets()
 
-    def _add(self, widget_cls, properties, handlers, sizer):
-        widget = widget_cls(self._container, properties)
-        self._sizer.Insert(self._sizer_index)
-        self._children.insert(self._child_index)
+    def _create_widget(self, widget_cls):
+        widget = widget_cls(self, {})
+        self.Sizer.Insert(self._sizer_index, widget, flag=wx.EXPAND, proportion=1)
+        self._sizer_index += 1
+        self._children.insert(self._child_index, widget)
+        self._child_index += 1
 
 class RLGuiFrame(wx.Frame, RLGuiContainerMixin):
-    pass
+
+    def __init__(self, parent, props):
+        wx.Frame.__init__(self, parent)
+        RLGuiContainerMixin.__init__(self, props)
+
+class RLGuiPanel(wx.Panel, RLGuiContainerMixin):
+
+    def __init__(self, parent, props):
+        wx.Panel.__init__(self, parent)
+        RLGuiContainerMixin.__init__(self, props)
 
 class MainFrame(RLGuiFrame):
-    def _get_defaults(self):
-        pass
+
+    def _get_props(self):
+        return {
+        }
+
     def _create_sizer(self):
         return wx.BoxSizer(wx.VERTICAL)
-    def _create(self):
+
+    def _create_widgets(self):
+        pass
+        self._create_widget(Toolbar)
+        self._create_widget(HBorder)
+        self._create_widget(Toolbar)
+
+class HBorder(RLGuiPanel):
+
+    def _get_props(self):
+        return {
+            'background': '#ff00ff',
+        }
+
+    def _create_sizer(self):
+        return wx.BoxSizer(wx.VERTICAL)
+
+    def _create_widgets(self):
+        pass
+
+class Toolbar(RLGuiPanel):
+
+    def _get_props(self):
+        return {
+            'background': '#00ffff',
+        }
+
+    def _create_sizer(self):
+        return wx.BoxSizer(wx.VERTICAL)
+
+    def _create_widgets(self):
         pass
 
 def load_document_from_file(path):
@@ -92,7 +151,7 @@ def load_json_from_file(path):
 
 def main():
     app = wx.App()
-    frame = MainFrame(None)
+    frame = MainFrame(None, {})
     frame.Show()
     app.MainLoop()
 
