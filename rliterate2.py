1d12a41dbc1b271822b2b3d08020074fd34eb48a
Rickard Lindberg <ricli85@gmail.com>
2019-10-22 21:51:13 +0200

    Click event behaves better.

--- rliterate2.py
+++ rliterate2.py
@@ -294,7 +294,6 @@ class RLGuiWxMixin(RLGuiMixin):
             self._bind_wx_event(wx.EVT_LEFT_UP, self._on_wx_left_up)
             self._bind_wx_event(wx.EVT_MOTION, self._on_wx_motion)
         if name == "click":
-            self._bind_wx_event(wx.EVT_LEFT_DOWN, self._on_wx_left_down)
             self._bind_wx_event(wx.EVT_LEFT_UP, self._on_wx_left_up)
 
     def _bind_wx_event(self, event, handler):
@@ -307,7 +306,7 @@ class RLGuiWxMixin(RLGuiMixin):
         self._call_event_handler("drag", DragEvent(True, 0))
 
     def _on_wx_left_up(self, wx_event):
-        if self._wx_down_pos is not None:
+        if self.HitTest(wx_event.Position) == wx.HT_WINDOW_INSIDE:
             self._call_event_handler("click", None)
         self._wx_down_pos = None
 
